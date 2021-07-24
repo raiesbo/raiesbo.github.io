471f05772f3444242391c38ee343d9f780798636
raiesbo <raiesbo8@gmail.com>
2021-07-24 17:48:43 +0200

    pop in content animation

---
+++ assets/animation.js
@@ -0,0 +1,12 @@
+const titles = document.querySelectorAll(".section-title");
+const bodies = document.querySelectorAll(".section-body");
+
+for (let i = 0; i < titles.length; i++) {
+    const title = titles[i];
+    title.style.animation = `popIn 400ms ease-out ${(400 * i) + 1200}ms`;
+    title.style.animationFillMode = "backwards";
+
+    const body = bodies[i];
+    body.style.animation = `popIn 400ms ease-out ${(400 * i) + 1400}ms`;
+    body.style.animationFillMode = "backwards";
+}
