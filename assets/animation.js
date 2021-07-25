8e62111117c68b2e24635bd543952a9cd5d9217a
raiesbo <raiesbo8@gmail.com>
2021-07-25 11:13:39 +0200

    detail correction + more info

--- assets/animation.js
+++ assets/animation.js
@@ -3,10 +3,10 @@ const bodies = document.querySelectorAll(".section-body");
 
 for (let i = 0; i < titles.length; i++) {
     const title = titles[i];
-    title.style.animation = `popIn 400ms ease-out ${(400 * i) + 1200}ms`;
+    title.style.animation = `popIn 400ms ease-out ${(400 * i) + 1600}ms`;
     title.style.animationFillMode = "backwards";
 
     const body = bodies[i];
-    body.style.animation = `popIn 400ms ease-out ${(400 * i) + 1400}ms`;
+    body.style.animation = `popIn 400ms ease-out ${(400 * i) + 1800}ms`;
     body.style.animationFillMode = "backwards";
 }
