02873e561334ea0387d971dbbd179c470421bb53
raiesbo <raiesbo@gmail.com>
2021-08-30 22:24:17 +0200

    Update details

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+export {default as contactLinksView} from "./contactLinksView.js";
+export {default as profileTranslator} from "./translator.js";
+export {default as skillsView} from "./skillsView.js";
+export {default as projectsView} from "./projectsView.js";
+export {default as educationView} from "./educationView.js";
+export {default as experienceView} from "./experienceView.js";
+export {default as certsView} from "./certsView.js";
+export {default as languagesView} from "./languagesView.js";
